4fbebc51f458a6f60a12cdb937b7e3f967496ac7
Rosco Kalis <roscokalis@gmail.com>
2022-08-19 15:27:12 +0200

    Add permits

--- lib/utils.tsx
+++ lib/utils.tsx
@@ -1,67 +1,209 @@
+declare let window: Window & {
+  ethereum: any;
+};
+
 // Approve a 100 DAI allowance to Uniswap
 export const buildExampleAllowanceTransaction = (address?: string) => ({
   from: address,
   to: '0x6b175474e89094c44da98b954eedeac495271d0f',
-  data: '0x095ea7b30000000000000000000000007a250d5630b4cf539739df2c5dacb4c659f2488d0000000000000000000000000000000000000000000000056bc75e2d63100000'
-})
+  data: '0x095ea7b30000000000000000000000007a250d5630b4cf539739df2c5dacb4c659f2488d0000000000000000000000000000000000000000000000056bc75e2d63100000',
+});
+
+export const buildExampleDaiPermit = (address?: string) => ({
+  types: {
+    EIP712Domain: [
+      { name: 'name', type: 'string' },
+      { name: 'version', type: 'string' },
+      { name: 'chainId', type: 'uint256' },
+      { name: 'verifyingContract', type: 'address' },
+    ],
+    Permit: [
+      { name: 'holder', type: 'address' },
+      { name: 'spender', type: 'address' },
+      { name: 'nonce', type: 'uint256' },
+      { name: 'expiry', type: 'uint256' },
+      { name: 'allowed', type: 'bool' },
+    ],
+  },
+  domain: {
+    name: 'Dai Stablecoin',
+    version: '1',
+    verifyingContract: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+    chainId: 1,
+  },
+  primaryType: 'Permit',
+  message: {
+    holder: address,
+    spender: '0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45',
+    allowed: true,
+    nonce: 0,
+    expiry: 1660916504,
+  },
+});
+
+export const buildExamplePermit = (address?: string) => ({
+  types: {
+    EIP712Domain: [
+      { name: 'name', type: 'string' },
+      { name: 'version', type: 'string' },
+      { name: 'chainId', type: 'uint256' },
+      { name: 'verifyingContract', type: 'address' },
+    ],
+    Permit: [
+      { name: 'owner', type: 'address' },
+      { name: 'spender', type: 'address' },
+      { name: 'value', type: 'uint256' },
+      { name: 'nonce', type: 'uint256' },
+      { name: 'deadline', type: 'uint256' },
+    ],
+  },
+  domain: {
+    name: 'USD Coin',
+    version: '2',
+    verifyingContract: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+    chainId: 1,
+  },
+  primaryType: 'Permit',
+  message: {
+    owner: address,
+    spender: '0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45',
+    value: '25000000',
+    nonce: 2,
+    deadline: 1660917549,
+  },
+});
 
 // List a CryptoDuckie for 1 ETH
 export const buildExampleOpenSeaListing = (address?: string) => ({
   types: {
     OrderComponents: [
-      { name: "offerer", type: "address" },
-      { name: "zone", type: "address" },
-      { name: "offer", type: "OfferItem[]" },
-      { name: "consideration", type: "ConsiderationItem[]" },
-      { name: "orderType", type: "uint8" },
-      { name: "startTime", type: "uint256" },
-      { name: "endTime", type: "uint256" },
-      { name: "zoneHash", type: "bytes32" },
-      { name: "salt", type: "uint256" },
-      { name: "conduitKey", type: "bytes32" },
-      { name: "counter", type: "uint256" }
+      { name: 'offerer', type: 'address' },
+      { name: 'zone', type: 'address' },
+      { name: 'offer', type: 'OfferItem[]' },
+      { name: 'consideration', type: 'ConsiderationItem[]' },
+      { name: 'orderType', type: 'uint8' },
+      { name: 'startTime', type: 'uint256' },
+      { name: 'endTime', type: 'uint256' },
+      { name: 'zoneHash', type: 'bytes32' },
+      { name: 'salt', type: 'uint256' },
+      { name: 'conduitKey', type: 'bytes32' },
+      { name: 'counter', type: 'uint256' },
     ],
     OfferItem: [
-      { name: "itemType", type: "uint8" },
-      { name: "token", type: "address" },
-      { name: "identifierOrCriteria", type: "uint256" },
-      { name: "startAmount", type: "uint256" },
-      { name: "endAmount", type: "uint256" }
+      { name: 'itemType', type: 'uint8' },
+      { name: 'token', type: 'address' },
+      { name: 'identifierOrCriteria', type: 'uint256' },
+      { name: 'startAmount', type: 'uint256' },
+      { name: 'endAmount', type: 'uint256' },
     ],
     ConsiderationItem: [
-      { name: "itemType", type: "uint8" },
-      { name: "token", type: "address" },
-      { name: "identifierOrCriteria", type: "uint256" },
-      { name: "startAmount", type: "uint256" },
-      { name: "endAmount", type: "uint256" },
-      { name: "recipient", type: "address" }
+      { name: 'itemType', type: 'uint8' },
+      { name: 'token', type: 'address' },
+      { name: 'identifierOrCriteria', type: 'uint256' },
+      { name: 'startAmount', type: 'uint256' },
+      { name: 'endAmount', type: 'uint256' },
+      { name: 'recipient', type: 'address' },
     ],
     EIP712Domain: [
-      { name: "name", type: "string" },
-      { name: "version", type: "string" },
-      { name: "chainId", type: "uint256" },
-      { name: "verifyingContract", type: "address" }
-    ]
+      { name: 'name', type: 'string' },
+      { name: 'version', type: 'string' },
+      { name: 'chainId', type: 'uint256' },
+      { name: 'verifyingContract', type: 'address' },
+    ],
   },
-  domain: { name: "Seaport", version: "1.1", chainId: "1", verifyingContract: "0x00000000006c3852cbef3e08e8df289169ede581" },
-  primaryType: "OrderComponents",
+  domain: {
+    name: 'Seaport',
+    version: '1.1',
+    chainId: '1',
+    verifyingContract: '0x00000000006c3852cbef3e08e8df289169ede581',
+  },
+  primaryType: 'OrderComponents',
   message: {
     offerer: address,
-    zone: "0x004c00500000ad104d7dbd00e3ae0a5c00560c00",
+    zone: '0x004c00500000ad104d7dbd00e3ae0a5c00560c00',
     offer: [
-      {itemType: "2", token: "0x922dc160f2ab743312a6bb19dd5152c1d3ecca33", identifierOrCriteria: "176", startAmount: "1", endAmount: "1" }
+      {
+        itemType: '2',
+        token: '0x922dc160f2ab743312a6bb19dd5152c1d3ecca33',
+        identifierOrCriteria: '176',
+        startAmount: '1',
+        endAmount: '1',
+      },
     ],
     consideration: [
-      { itemType: "0", token: "0x0000000000000000000000000000000000000000", identifierOrCriteria: "0", startAmount: "925000000000000000", endAmount: "925000000000000000", recipient: address },
-      { itemType: "0", token: "0x0000000000000000000000000000000000000000", identifierOrCriteria: "0", startAmount: "25000000000000000", endAmount: "25000000000000000", recipient: "0x8de9c5a032463c561423387a9648c5c7bcc5bc90" },
-      { itemType: "0", token: "0x0000000000000000000000000000000000000000", identifierOrCriteria: "0", startAmount: "50000000000000000", endAmount: "50000000000000000", recipient: "0x5c6139cd9ff1170197f13935c58f825b422c744c" }
+      {
+        itemType: '0',
+        token: '0x0000000000000000000000000000000000000000',
+        identifierOrCriteria: '0',
+        startAmount: '0',
+        endAmount: '0',
+        recipient: address,
+      },
+      {
+        itemType: '0',
+        token: '0x0000000000000000000000000000000000000000',
+        identifierOrCriteria: '0',
+        startAmount: '25000000000000000',
+        endAmount: '25000000000000000',
+        recipient: '0x8de9c5a032463c561423387a9648c5c7bcc5bc90',
+      },
+      {
+        itemType: '0',
+        token: '0x0000000000000000000000000000000000000000',
+        identifierOrCriteria: '0',
+        startAmount: '50000000000000000',
+        endAmount: '50000000000000000',
+        recipient: '0x5c6139cd9ff1170197f13935c58f825b422c744c',
+      },
     ],
-    orderType: "3",
-    startTime: "1660565524",
-    endTime: "1661170320",
-    zoneHash: "0x3000000000000000000000000000000000000000000000000000000000000000",
-    salt: "5965482869793190759363249887602871532",
-    conduitKey: "0x0000007b02230091a7ed01230072f7006a004d60a8d4e71d599b8104250f0000",
-    counter: "0",
+    orderType: '3',
+    startTime: '1660565524',
+    endTime: '1661170320',
+    zoneHash: '0x3000000000000000000000000000000000000000000000000000000000000000',
+    salt: '5965482869793190759363249887602871532',
+    conduitKey: '0x0000007b02230091a7ed01230072f7006a004d60a8d4e71d599b8104250f0000',
+    counter: '0',
+  },
+});
+
+export const request = async (method: string, params: any[]) => {
+  try {
+    const res = await window.ethereum.request({ method, params });
+    console.log(res);
+  } catch (err) {
+    console.log(err);
   }
-})
+};
+
+export const sendCallback = async (method: string, params: any[]) => {
+  window.ethereum.send({ method, params }, (err: any, res: any) => {
+    console.log('err', err);
+    console.log('res', res);
+  });
+};
+
+export const sendPromise = async (method: string, params: any[]) => {
+  try {
+    const res = await window.ethereum.send(method, params);
+    console.log(res);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const sendAsync = async (method: string, params: any[]) => {
+  window.ethereum.sendAsync({ method, params }, (err: any, res: any) => {
+    console.log('err', err);
+    console.log('res', res);
+  });
+};
+
+export const bypass = async (method: string, params: any[]) => {
+  window.postMessage({
+    target: 'metamask-contentscript',
+    data: {
+      name: 'metamask-provider',
+      data: { method, params },
+    },
+  });
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
-import { providers } from 'ethers'
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import { useEffect, useState } from 'react'
-import { buildExampleAllowanceTransaction, buildExampleOpenSeaListing } from '../lib/utils'
+import { providers } from 'ethers';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import { useEffect, useState } from 'react';
+import { buildExampleAllowanceTransaction, buildExampleDaiPermit, buildExampleOpenSeaListing, buildExamplePermit, bypass, request, sendAsync, sendCallback, sendPromise } from '../lib/utils';
 
 declare let window: Window & {
   ethereum: any;
-}
+};
 
 const KicthenSink: NextPage = () => {
   const [address, setAddress] = useState<string>();
@@ -24,115 +24,25 @@ const KicthenSink: NextPage = () => {
     const provider = new providers.Web3Provider(window.ethereum);
     const [newAddress] = await provider.listAccounts();
     setAddress(newAddress);
-  }
+  };
 
-  const allowanceRequest = async () => {
-    try {
-      const res = await window.ethereum.request({
-        method: 'eth_sendTransaction',
-        params: [buildExampleAllowanceTransaction(address)]
-      })
-      console.log(res);
-    } catch (err) {
-      console.log(err)
-    }
-  }
+  const allowanceRequest = () => request('eth_sendTransaction', [buildExampleAllowanceTransaction(address)]);
+  const allowanceSendCallback = () => sendCallback('eth_sendTransaction', [buildExampleAllowanceTransaction(address)]);
+  const allowanceSendPromise = () => sendPromise('eth_sendTransaction', [buildExampleAllowanceTransaction(address)]);
+  const allowanceSendAsync = () => sendAsync('eth_sendTransaction', [buildExampleAllowanceTransaction(address)]);
+  const allowanceBypass = () => bypass('eth_sendTransaction', [buildExampleAllowanceTransaction(address)]);
 
-  const allowanceSendCallback = async () => {
-    window.ethereum.send(
-      { method: 'eth_sendTransaction', params: [buildExampleAllowanceTransaction(address)] },
-      (err: any, res: any) => {
-        console.log('err', err);
-        console.log('res', res);
-      }
-    )
-  }
+  const openseaListingRequest = () => request('eth_signTypedData_v3', [address, JSON.stringify(buildExampleOpenSeaListing(address))]);
+  const openseaListingSendCallback = () => sendCallback('eth_signTypedData_v4', [address, JSON.stringify(buildExampleOpenSeaListing(address))]);
+  const openseaListingSendPromise = () => sendPromise('eth_signTypedData_v3', [address, JSON.stringify(buildExampleOpenSeaListing(address))]);
+  const openseaListingSendAsync = () => sendAsync('eth_signTypedData_v4', [address, JSON.stringify(buildExampleOpenSeaListing(address))]);
+  const openseaListingBypass = () => bypass('eth_signTypedData_v3', [address, JSON.stringify(buildExampleOpenSeaListing(address))]);
 
-  const allowanceSendPromise = async () => {
-    try {
-      const res = await window.ethereum.send('eth_sendTransaction', [buildExampleAllowanceTransaction(address)]);
-      console.log(res);
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
-  const allowanceSendAsync = async () => {
-    window.ethereum.sendAsync({
-      method: 'eth_sendTransaction',
-      params: [buildExampleAllowanceTransaction(address)]
-    }, (err: any, res: any) => {
-      console.log('err', err);
-      console.log('res', res);
-    })
-  }
-
-  const allowanceBypass = async () => {
-    window.postMessage({
-      target: 'metamask-contentscript',
-      data: {
-        name: 'metamask-provider',
-        data: {
-          method: 'eth_sendTransaction',
-          params: [buildExampleAllowanceTransaction(address)]
-        }
-      }
-    })
-  }
-
-  const openseaListingRequest = async () => {
-    try {
-      const res = await window.ethereum.request({
-        method: 'eth_signTypedData_v3',
-        params: [address, JSON.stringify(buildExampleOpenSeaListing(address))]
-      })
-      console.log(res);
-    } catch (err) {
-      console.log(err)
-    }
-  }
-
-  const openseaListingSendCallback = async () => {
-    window.ethereum.send(
-      { method: 'eth_signTypedData_v4', params: [address, JSON.stringify(buildExampleOpenSeaListing(address))] },
-      (err: any, res: any) => {
-        console.log('err', err);
-        console.log('res', res);
-      }
-    )
-  }
-
-  const openseaListingSendPromise = async () => {
-    try {
-      const res = await window.ethereum.send('eth_signTypedData_v3', [address, JSON.stringify(buildExampleOpenSeaListing(address))]);
-      console.log(res);
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
-  const openseaListingSendAsync = async () => {
-    window.ethereum.sendAsync({
-      method: 'eth_signTypedData_v4',
-      params: [address, JSON.stringify(buildExampleOpenSeaListing(address))]
-    }, (err: any, res: any) => {
-      console.log('err', err);
-      console.log('res', res);
-    })
-  }
-
-  const openseaListingBypass = async () => {
-    window.postMessage({
-      target: 'metamask-contentscript',
-      data: {
-        name: 'metamask-provider',
-        data: {
-          method: 'eth_signTypedData_v3',
-          params: [address, JSON.stringify(buildExampleOpenSeaListing(address))]
-        }
-      }
-    })
-  }
+  const permitRequest = () => request('eth_signTypedData_v3', [address, JSON.stringify(buildExampleDaiPermit(address))]);
+  const permitSendCallback = () => sendCallback('eth_signTypedData_v4', [address, JSON.stringify(buildExampleDaiPermit(address))]);
+  const permitSendPromise = () => sendPromise('eth_signTypedData_v3', [address, JSON.stringify(buildExamplePermit(address))]);
+  const permitSendAsync = () => sendAsync('eth_signTypedData_v4', [address, JSON.stringify(buildExamplePermit(address))]);
+  const permitBypass = () => bypass('eth_signTypedData_v3', [address, JSON.stringify(buildExamplePermit(address))]);
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
@@ -142,26 +52,72 @@ const KicthenSink: NextPage = () => {
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-start px-20 text-center gap-2">
-        <button className='border border-black p-2' onClick={connect}>{address ?? 'Connect'}</button>
+        <button className="border border-black p-2" onClick={connect}>
+          {address ?? 'Connect'}
+        </button>
         {address && <div>Allowances</div>}
-        {address && <div className="flex w-full items-center justify-center px-20 text-center gap-2">
-          <button className='border border-black p-2' onClick={allowanceRequest}>ethereum.request</button>
-          <button className='border border-black p-2' onClick={allowanceSendCallback}>ethereum.send (callback)</button>
-          <button className='border border-black p-2' onClick={allowanceSendPromise}>ethereum.send (promise)</button>
-          <button className='border border-black p-2' onClick={allowanceSendAsync}>ethereum.sendAsync</button>
-          <button className='border border-black p-2' onClick={allowanceBypass}>bypass</button>
-        </div>}
+        {address && (
+          <div className="flex w-full items-center justify-center px-20 text-center gap-2">
+            <button className="border border-black p-2" onClick={allowanceRequest}>
+              ethereum.request
+            </button>
+            <button className="border border-black p-2" onClick={allowanceSendCallback}>
+              ethereum.send (callback)
+            </button>
+            <button className="border border-black p-2" onClick={allowanceSendPromise}>
+              ethereum.send (promise)
+            </button>
+            <button className="border border-black p-2" onClick={allowanceSendAsync}>
+              ethereum.sendAsync
+            </button>
+            <button className="border border-black p-2" onClick={allowanceBypass}>
+              bypass
+            </button>
+          </div>
+        )}
         {address && <div>NFT Listings</div>}
-        {address && <div className="flex w-full items-center justify-center px-20 text-center gap-2">
-          <button className='border border-black p-2' onClick={openseaListingRequest}>ethereum.request</button>
-          <button className='border border-black p-2' onClick={openseaListingSendCallback}>ethereum.send (callback)</button>
-          <button className='border border-black p-2' onClick={openseaListingSendPromise}>ethereum.send (promise)</button>
-          <button className='border border-black p-2' onClick={openseaListingSendAsync}>ethereum.sendAsync</button>
-          <button className='border border-black p-2' onClick={openseaListingBypass}>bypass</button>
-        </div>}
+        {address && (
+          <div className="flex w-full items-center justify-center px-20 text-center gap-2">
+            <button className="border border-black p-2" onClick={openseaListingRequest}>
+              ethereum.request
+            </button>
+            <button className="border border-black p-2" onClick={openseaListingSendCallback}>
+              ethereum.send (callback)
+            </button>
+            <button className="border border-black p-2" onClick={openseaListingSendPromise}>
+              ethereum.send (promise)
+            </button>
+            <button className="border border-black p-2" onClick={openseaListingSendAsync}>
+              ethereum.sendAsync
+            </button>
+            <button className="border border-black p-2" onClick={openseaListingBypass}>
+              bypass
+            </button>
+          </div>
+        )}
+        {address && <div>Permit</div>}
+        {address && (
+          <div className="flex w-full items-center justify-center px-20 text-center gap-2">
+            <button className="border border-black p-2" onClick={permitRequest}>
+              ethereum.request
+            </button>
+            <button className="border border-black p-2" onClick={permitSendCallback}>
+              ethereum.send (callback)
+            </button>
+            <button className="border border-black p-2" onClick={permitSendPromise}>
+              ethereum.send (promise)
+            </button>
+            <button className="border border-black p-2" onClick={permitSendAsync}>
+              ethereum.sendAsync
+            </button>
+            <button className="border border-black p-2" onClick={permitBypass}>
+              bypass
+            </button>
+          </div>
+        )}
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default KicthenSink
+export default KicthenSink;
